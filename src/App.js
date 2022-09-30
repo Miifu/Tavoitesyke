c103a62571e2eacf8d7a6ab90cef7bd9f116f70f
Miifu <mil_la2000@hotmail.com>
2022-09-30 11:06:32 +0300

    Tavoitesykkeen tehtäväpalautus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [age, setAge] = useState(0)
+const [upper, setUpper] = useState(0)
+const [lower, setLower] = useState(0)
+
+function laske(e){
+  e.preventDefault()
+  const tulos1 = (220 - age) * 0.85
+  const tulos2 = (220 - age) * 0.65
+  setUpper(tulos1)
+  setLower(tulos2)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h3>BMI</h3>
+      <form onSubmit={laske}>
+        <div>
+          <label>Age</label>
+          <input value={age} onChange={e => setAge(e.target.value)}/>
+          </div>
+          <div>
+          <label>Heart rate limits</label>
+          <output>{lower.toFixed(1)}-{upper.toFixed(1)}</output>
+        </div>
+        <button>Laske</button>
+      </form>
     </div>
   );
 }
